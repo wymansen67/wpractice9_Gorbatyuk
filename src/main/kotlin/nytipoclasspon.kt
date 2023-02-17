9a268f7951fccc7886de2aad8876a073d432d6f8
wymansen67 <DenisGorbatuk77@gmail.com>
2023-02-17 13:24:07 +0500

    Fixes

--- src/main/kotlin/nytipoclasspon.kt
+++ src/main/kotlin/nytipoclasspon.kt
@@ -1,12 +1,51 @@
 package vottipoklaas
-class Math(var pass_score:UByte, var info: String) {
-    fun udacha () {
+
+class Math(
+    var pass_score: UByte,
+    var info: String,
+    var max_score: UByte,
+    var debts: Boolean,
+    var admisson: Boolean,
+    var pass: String,
+    var score: UByte
+) {
+    fun udacha() {
         println("Ну короче надо $pass_score для получения $info")
+    }
+
+    fun getPass() {
+        if (score <= max_score || score > pass_score) {
+            pass = "Молодец блин. Сдал. Чтоб больше я тебя здесь не видел."
+        } else pass = "Плохо блин. Не сдал. Прибегай еще."
+    }
+
+    fun getAdmission() {
+        if (!debts) admisson = true
+        else admisson = false
     }
 }
 
-class Rusish(var pass_score:UByte, var info: String) {
-    fun udacha () {
+class Rusish(
+    var pass_score: UByte,
+    var info: String,
+    var max_score: UByte,
+    var debts: Boolean,
+    var admisson: Boolean,
+    var pass: String,
+    var score: UByte
+) {
+    fun udacha() {
         println("Ну короче надо $pass_score для получения $info")
+    }
+
+    fun getPass() {
+        if (score <= max_score || score > pass_score) {
+            pass = "Молодец блин. Сдал. Чтоб больше я тебя здесь не видел."
+        } else pass = "Плохо блин. Не сдал. Прибегай еще."
+    }
+
+    fun getAdmission() {
+        if (!debts) admisson = true
+        else admisson = false
     }
 }
--- src/main/kotlin/votischevamclass.kt
+++ src/main/kotlin/votischevamclass.kt
@@ -1,11 +1,42 @@
 package edem
-class Old (var how_old:String, var how_long:UInt ) {
-    fun gazeta () {
-        println("Возраст газеты целых $how_old размера $how_long")
+
+class Old(var how_old: String, var length: UInt, var summary: String, var cost: UInt) {
+    fun gazeta() {
+        println("Возраст газеты целых $how_old размера $length")
     }
+
+    fun getContext() {
+        if (summary != "") {
+            println("Краткое содержание: $summary")
+        } else summary = "No data"
+    }
+
+    fun getCost() {
+        if (cost != 0u) {
+            if (length < 100u) cost = 10u
+            else if (length >= 100u && length < 500u) cost = 30u
+            else if (length >= 500u && length < 1000u) cost = 50u
+        }
+    }
+
 }
-class New (var how_young:String, var how_long:UInt ) {
-    fun gazeta () {
-        println("Возраст газеты всего $how_young размера $how_long")
+
+class New(var how_young: String, var length: UInt, var summary: String, var cost: UInt) {
+    fun gazeta() {
+        println("Возраст газеты всего $how_young размера $length")
+    }
+
+    fun getContext() {
+        if (summary != "") {
+            println("Краткое содержание: $summary")
+        } else summary = "No data"
+    }
+
+    fun getCost() {
+        if (cost != 0u) {
+            if (length < 100u) cost = 10u
+            else if (length >= 100u && length < 500u) cost = 30u
+            else if (length >= 500u && length < 1000u) cost = 50u
+        }
     }
 }
